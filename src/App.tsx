b8222cce3f7e78d17b4426cd94af592bafdaff56
midCenturyDude7 <mgriffes@gmail.com>
2022-08-27 11:17:40 -0700

    Updated configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming soon: Trello Clone!
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+  return null;
 }
-
-export default App;
