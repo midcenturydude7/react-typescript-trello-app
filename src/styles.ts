8b6c149102621ace81df864e710bcd8c66bb48b9
midCenturyDude7 <mgriffes@gmail.com>
2022-08-27 21:00:34 -0700

    Working with state to add lists and cards

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const AppContainer = styled.div`
   align-items: flex-start;
   background-color: #3179ba;
@@ -34,4 +33,24 @@ export const CardContainer = styled.div`
   max-width: 300px;
   border-radius: 3px;
   box-shadow: #091e4240 0px 1px 0px 0px;
+`
+
+type AddItemButtonProps = {
+  dark?: boolean;
+}
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${(props) => (props.dark ? "#000" : "#fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffffff52;
+  }
 `
