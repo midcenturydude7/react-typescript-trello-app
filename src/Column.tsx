c9303c822250801489c0613f5e438fe0679d3321
midCenturyDude7 <mgriffes@gmail.com>
2022-08-28 12:50:25 -0700

    Implemented global state

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,26 @@
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { useAppState } from "./state/AppStateContext";
 
 type ColumnProps = {
-  text: string
+  text: string;
+  id: string;
 }
 
-export const Column = ({text}: ColumnProps) => {
+export const Column = ({text, id}: ColumnProps) => {
+  const { getTasksByListId } = useAppState();
+
+  const tasks = getTasksByListId(id)
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold" />
-      <Card text="Learn Typescript" />
-      <Card text="Begin to use static typing" />
+      {
+        tasks.map(task => (
+          <Card text={task.text} key={task.id} id={task.id} />
+        ))
+      }
       <AddNewItem 
         toggleButtonText="+ Add another card"
         onAdd={() => console.log("New item added")}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
+import { AppStateProvider } from './state/AppStateContext';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
